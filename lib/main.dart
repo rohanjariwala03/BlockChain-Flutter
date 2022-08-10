9465d2a2a62e348b4c6d8716a12c00413ab2078a
rohanjariwala <rohanjariwala03@gmail.com>
2022-08-10 16:24:53 -0400

    main file changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,129 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart';
+import 'package:web3dart/web3dart.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Smart Contract',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  int balance = 0, totalDeposits = 0, installmentAmount = 3;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  late Client httpClient;
+  late Web3Client ethClient;
+  // JSON-RPC is a remote procedure call protocol encoded in JSON
+  // Remote Procedure Call (RPC) is about executing a block of code on another server
+  String rpcUrl = 'http://0.0.0.0:7545';
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+  void initState() {
+    initialSetup();
+    super.initState();
+  }
+
+  Future<void> initialSetup() async {
+    /// This will start a client that connects to a JSON RPC API, available at RPC URL.
+    /// The httpClient will be used to send requests to the [RPC server].
+    httpClient = Client();
+
+    /// It connects to an Ethereum [node] to send transactions, interact with smart contracts, and much more!
+    ethClient = Web3Client(rpcUrl, httpClient);
+
+    await getCredentials();
+    await getDeployedContract();
+    await getContractFunctions();
+  }
+
+  /// This will construct [credentials] with the provided [privateKey]
+  /// and load the Ethereum address in [myAdderess] specified by these credentials.
+  String privateKey =
+      'af06facb9d45d228c301a0d9bd286fdee2be38c3138f65e8f521c99ca12b1810';
+  late Credentials credentials;
+  late EthereumAddress myAddress;
+
+  Future<void> getCredentials() async {
+    credentials = await ethClient.credentialsFromPrivateKey(privateKey);
+    myAddress = await credentials.extractAddress();
+  }
+
+  /// This will parse an Ethereum address of the contract in [contractAddress]
+  /// from the hexadecimal representation present inside the [ABI]
+  late String abi;
+  late EthereumAddress contractAddress;
+
+  Future<void> getDeployedContract() async {
+    String abiString = await rootBundle.loadString('src/abis/Investment.json');
+    var abiJson = jsonDecode(abiString);
+    abi = jsonEncode(abiJson['abi']);
+
+    contractAddress =
+        EthereumAddress.fromHex(abiJson['networks']['5777']['address']);
+  }
+
+  /// This will help us to find all the [public functions] defined by the [contract]
+  late DeployedContract contract;
+  late ContractFunction getBalanceAmount,
+      getDepositAmount,
+      addDepositAmount,
+      withdrawBalance;
+
+  Future<void> getContractFunctions() async {
+    contract = DeployedContract(
+        ContractAbi.fromJson(abi, "Investment"), contractAddress);
+
+    getBalanceAmount = contract.function('getBalanceAmount');
+    getDepositAmount = contract.function('getDepositAmount');
+    addDepositAmount = contract.function('addDepositAmount');
+    withdrawBalance = contract.function('withdrawBalance');
+  }
+
+  /// This will call a [functionName] with [functionArgs] as parameters
+  /// defined in the [contract] and returns its result
+  Future<List<dynamic>> readContract(
+      ContractFunction functionName,
+      List<dynamic> functionArgs,
+      ) async {
+    var queryResult = await ethClient.call(
+      contract: contract,
+      function: functionName,
+      params: functionArgs,
+    );
+
+    return queryResult;
+  }
+
+
+/// Signs the given transaction using the keys supplied in the [credentials] object
+/// to upload it to the client so that it can be executed
+  Future<void> writeContract(
+      ContractFunction functionName,
+      List<dynamic> functionArgs,
+      ) async {
+    await ethClient.sendTransaction(
+      credentials,
+      Transaction.callContract(
+        contract: contract,
+        function: functionName,
+        parameters: functionArgs,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-}
